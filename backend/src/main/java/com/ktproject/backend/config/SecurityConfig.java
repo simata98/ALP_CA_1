7b1da8819393be8470e8e8e40a4553ed97e36add
simata98 <wlsrnr905@naver.com>
2024-09-06 17:22:51 +0900

    Update SecurityConfig.java

--- backend/src/main/java/com/ktproject/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/ktproject/backend/config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
         .csrf(csrf -> csrf.disable())
         .authorizeRequests(aythorizeRequests -> aythorizeRequests
             .requestMatchers("/auth/register", "/auth/login").permitAll()
+            .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/swagger-resources/**").permitAll()
             .anyRequest().authenticated())
         .addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
         .logout(logout -> logout
